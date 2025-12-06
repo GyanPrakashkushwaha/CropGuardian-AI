9e4798a4e25b022a582c07bdcde02119399d8194
GyanPrakashkushwaha <gyanp7880@gmail.com>
2025-12-06 22:32:07 +0530

    creating a functional backend again

--- backend/app/routes/weather_route.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from backend.app.services.weather_simple import get_static_weather
-
-router = APIRouter()
-
-@router.get("/weather")
-async def weather():
-    data = await get_static_weather()
-    return {"weather": data}
